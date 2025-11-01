b8b5572328cc0898d1bfd12a27076210d060c27b
taweeratWatchara156 <taweeratmain156@gmail.com>
2025-11-01 12:57:30 +0700

    add remember username feature

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@
 import useAuthen from '@/components/global_states/useAuth'
 import useLoading from '@/components/global_states/useLoading'
 import { Button, Checkbox, Input, Space } from 'antd'
+import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
 import React, { FormEvent, useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
@@ -12,6 +13,7 @@ export default function LoginPage() {
     const router = useRouter()
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [rememberUsername, setRememberUsername] = useState(false)
     const setLoading = useLoading(s => s.setLoading)
     const setLogin = useAuthen(s => s.setLogin)
 
@@ -19,6 +21,12 @@ export default function LoginPage() {
         const token = localStorage.getItem("token")
         if (token){
             router.push("/")
+        }
+
+        const rememberUsername = Cookies.get("username")
+        if (rememberUsername){
+            setUsername(rememberUsername)
+            setRememberUsername(true)
         }
     }, [])
 
@@ -40,6 +48,12 @@ export default function LoginPage() {
             const data = await res.json()
 
             if (res.ok){
+                if (rememberUsername) {
+                    Cookies.set("username", username, { expires: 7 })
+                }else{
+                    Cookies.remove("username")
+                }
+
                 setLoading(false)
                 setLogin(data.token)
                 toast.success(data.message)
@@ -59,14 +73,14 @@ export default function LoginPage() {
         <form onSubmit={handleSubmit} className='w-full sm:w-auto px-5 md:px-10 py-10 flex flex-col gap-5 bg-[#86B0BD] rounded-md text-white shadow-xl border-[#67828b] border-2 border-dotted'>
             <h1 className='font-bold text-2xl sm:text-3xl mx-auto'>Login</h1>
             <div className='hidden sm:flex flex-col gap-3 w-[400px]'>
-                <Input onChange={(e) => setUsername(e.target.value)} size="large" style={{color: "#3f5860"}} placeholder='username' prefix={<FaUser className='text-[#3f5860]' />}/>
+                <Input value={username} onChange={(e) => setUsername(e.target.value)} size="large" style={{color: "#3f5860"}} placeholder='username' prefix={<FaUser className='text-[#3f5860]' />}/>
                 <Input.Password onChange={(e) => setPassword(e.target.value)} size="large" style={{color: "#3f5860"}} placeholder='password' prefix={<FaLock className='text-[#3f5860]' />}/>
-                <Checkbox style={{color: "white"}}>Remember Username</Checkbox>
+                <Checkbox style={{color: "white"}} checked={rememberUsername} onChange={(e) => setRememberUsername(e.target.checked)}>Remember Username</Checkbox>
             </div>
             <div className='flex sm:hidden flex-col gap-3 w-full'>
-                <Input onChange={(e) => setUsername(e.target.value)} size="middle" style={{color: "#3f5860"}} placeholder='username' prefix={<FaUser className='text-[#3f5860]' />}/>
+                <Input value={username} onChange={(e) => setUsername(e.target.value)} size="middle" style={{color: "#3f5860"}} placeholder='username' prefix={<FaUser className='text-[#3f5860]' />}/>
                 <Input.Password onChange={(e) => setPassword(e.target.value)} size="middle" style={{color: "#3f5860"}} placeholder='password' prefix={<FaLock className='text-[#3f5860]' />}/>
-                <Checkbox style={{color: "white"}}>Remember Username</Checkbox>
+                <Checkbox style={{color: "white"}} checked={rememberUsername} onChange={(e) => setRememberUsername(e.target.checked)}>Remember Username</Checkbox>
             </div>
 
             <div className='hidden sm:flex'><Button htmlType='submit' size='large' style={{backgroundColor: "#E2A16F", border: "#a06f49", color:"white"}} className='w-full'>Login</Button></div>
